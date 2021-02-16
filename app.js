9d3d13c3b281f7fece773ab8f2591abbd118b8d3
sazaam <sazaam@gmail.com>
2021-02-16 19:28:43 +0900

    first try to update stuffs

--- app.js
+++ app.js
@@ -1,186 +1,187 @@
-require('dotenv').config() ;
+require('dotenv').config();
 
-const express = require('express')  ;
+const express = require('express');
 
-let async = require('express-async-await') ;
-let createError = require('http-errors') ;
-let cookieParser = require('cookie-parser') ;
+let async = require('express-async-await');
+let createError = require('http-errors');
+let cookieParser = require('cookie-parser');
 
-let path = require('path') ;
+let path = require('path');
 
-let fs = require('fs') ;
-let fetch = require('node-fetch') ;
-let axios = require('axios') ;
-const { GraphQLClient, gql } = require('graphql-request') ;
+let fs = require('fs');
+let fetch = require('node-fetch');
+let axios = require('axios');
+const { GraphQLClient, gql } = require('graphql-request');
 // view engine
-const jade = require('jade') ;
-const md = require('marked') ;
+const jade = require('jade');
+const md = require('marked');
 
-const server = require('./server') ;
+const server = require('./server');
 
-const i18 = require('./lang') ;
-const queries = require('./queries') ;
-const UTILS = require('./utils') ;
-const CONSTANTS = require('./constants') ;
+const i18 = require('./lang');
+const queries = require('./queries');
+const UTILS = require('./utils');
+const CONSTANTS = require('./constants');
 
 
 // let isLive = process.env.LIVE ;
-let isLive = !!process.env.LIVE ;
+let isLive = process.env.LIVE == 1;
 
 // START THE EXPRESS
-const app = express() ;
+const app = express();
 
 
 // CONFIGURE EXPRESS APP
 (app => {
 
-	// FIRST SETTINGS
-	
-	// internationalization before view engine
-	app.use(i18.enable()) ;
+    // FIRST SETTINGS
 
-	// view engine setup to jade
-	app.set('views', path.join(__dirname, 'public', 'jade')) ;
-	app.set('view engine', 'jade') ;
-	// app.set('view cache', true);
+    // internationalization before view engine
+    app.use(i18.enable());
 
-	// basic setup
-	app.use(express.json()) ;
-	app.use(express.urlencoded({ extended: false })) ;
-	app.use(cookieParser()) ;
+    // view engine setup to jade
+    app.set('views', path.join(__dirname, 'public', 'jade'));
+    app.set('view engine', 'jade');
+    // app.set('view cache', true);
 
-	app.use(express.static(path.join(__dirname, 'public'))) ;
+    // basic setup
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 
-})(app) ;
+    app.use(express.static(path.join(__dirname, 'public')));
+
+})(app);
 
 
 
 // DKT VIEWER (FRONT-END) USER DB LOGIN STORAGE
-let login = async (app) => {
-	// No unhandled rejection!
-	console.log('////////  LOGGING IN AS DKT ////////')
+let login = async(app) => {
+    // No unhandled rejection!
+    console.log('////////  LOGGING IN AS DKT ////////')
 
-	const { data } = await axios.post(CONSTANTS.PATH.db + CONSTANTS.PATH.db_auth, CONSTANTS.users.dkt) ;
-	
-	CONSTANTS.user = data ;
-	CONSTANTS.token = 'Bearer ' + data.jwt ;
+    const { data } = await axios.post(CONSTANTS.PATH.db + CONSTANTS.PATH.db_auth, CONSTANTS.users.dkt);
 
-	const endpoint = CONSTANTS.PATH.db + CONSTANTS.PATH.db_graphql ;
-	const { GraphQLClient } = require('graphql-request');
-	const { Headers } = require('cross-fetch');
-	
-	global.Headers = global.Headers || Headers;
-	
-	CONSTANTS.DKTClient = new GraphQLClient(endpoint, {
-		headers: {
-			authorization: `Bearer ${CONSTANTS.user.jwt}`
-		},
-	});
-	
-	console.log('\t  LOGIN SUCCESSFULL') ;
+    CONSTANTS.user = data;
+    CONSTANTS.token = 'Bearer ' + data.jwt;
+
+    const endpoint = CONSTANTS.PATH.db + CONSTANTS.PATH.db_graphql;
+    const { GraphQLClient } = require('graphql-request');
+    const { Headers } = require('cross-fetch');
+
+    global.Headers = global.Headers || Headers;
+
+    CONSTANTS.DKTClient = new GraphQLClient(endpoint, {
+        headers: {
+            authorization: `Bearer ${CONSTANTS.user.jwt}`
+        },
+    });
+
+    console.log('\t LOGIN SUCCESSFULL');
 }
 
 // DB OR FIXTURES
-let fetchdata = async (req, res, part, noreturn) => {
-	let data ;
-	if(isLive) {
-		data = await CONSTANTS.DKTClient.request(queries.sections[part], queries.sections.variables || {}) ;
-		data = data.sections ;
-	}else{
-		data = await fs.promises.readFile(CONSTANTS.fixtures[part], 'utf8') ;
-		data = UTILS.formatHashes( JSON.parse( data ) ) ;
-	}
+let fetchdata = async(req, res, part, noreturn) => {
+    let data;
+    if (isLive) {
+        data = await CONSTANTS.DKTClient.request(queries.sections[part], queries.sections.variables || {});
+        data = data.sections;
+    } else {
+        data = await fs.promises.readFile(CONSTANTS.fixtures[part], 'utf8');
+        data = UTILS.formatHashes(JSON.parse(data));
+    }
 
-	if(noreturn) res.json(data) ;
-	else return data ;
+    if (noreturn) res.json(data);
+    else return data;
 }
 
 
 
 // JADESETTINGS
-const jadebasedir = path.join(__dirname, 'public', 'jade') ;
+const jadebasedir = path.join(__dirname, 'public', 'jade');
 const JADESETTINGS = {
-	params:{},
-	excludes:{
-		// settings:1,
-		// language:1,
-		// languageDir:1,
-		// t:1,
-		// exists:1,
-		i18n:1,
-		// basedir:1,
-		// title:1,
-		// lang:1,
-		// render:1,
-		// renderFile:1,
-		// join:1,
-		// p:1,
-		_locals:1,
-		cache:1,
-		// filename:1
-	}
+    params: {},
+    excludes: {
+        // settings:1,
+        // language:1,
+        // languageDir:1,
+        // t:1,
+        // exists:1,
+        i18n: 1,
+        // basedir:1,
+        // title:1,
+        // lang:1,
+        // render:1,
+        // renderFile:1,
+        // join:1,
+        // p:1,
+        _locals: 1,
+        cache: 1,
+        // filename:1
+    }
 
 }
-let i18next = i18.i18next ;
+let i18next = i18.i18next;
 
 let merge = (p, newp) => {
-	for(var s in newp) p[s] = newp[s] ;
-	return p ;
+    for (var s in newp) p[s] = newp[s];
+    return p;
 }
 
 let clone = (p) => {
-	let cl = {}, ex = JADESETTINGS.excludes ;
-	for(var s in p){
-		if(!(s in ex)){
-			cl[s] = p[s] ;
-		}
-	}
-	return cl ;
+    let cl = {},
+        ex = JADESETTINGS.excludes;
+    for (var s in p) {
+        if (!(s in ex)) {
+            cl[s] = p[s];
+        }
+    }
+    return cl;
 }
 
 let p = (input, locals) => {
-	return merge(clone(input), clone(locals)) ;
+    return merge(clone(input), clone(locals));
 }
 
 let customize = (bracket, source) => {
-	var customs = {} ;
-	var module = getComponentsByTypename(bracket, 'ComponentJadeJadePage') ;
-	if(!! module.length && module[0].jade != ''){
-		customs = rfs(module[0].jade, module[0].path, source) ;
-	}
-	return customs ;
+    var customs = {};
+    var module = getComponentsByTypename(bracket, 'ComponentJadeJadePage');
+    if (!!module.length && module[0].jade != '') {
+        customs = rfs(module[0].jade, module[0].path, source);
+    }
+    return customs;
 }
 
 let rfs = (src, filename, params) => {
-	var Module = module.constructor;
-	var m = new Module("", params) ;
-	
-	m._compile(src, filename) ;
-	return m.exports ;
+    var Module = module.constructor;
+    var m = new Module("", params);
+
+    m._compile(src, filename);
+    return m.exports;
 }
 
 let getComponentsByTypename = (list, name) => {
-	let l = list.length ;
-	let p = [] ;
-	for(var i = 0 ; i < l ; i ++){
-		let el = list[i] ;
-		if(name == el.__typename) 
-			p[p.length] = el ;
-	}
-	return p ;
+    let l = list.length;
+    let p = [];
+    for (var i = 0; i < l; i++) {
+        let el = list[i];
+        if (name == el.__typename)
+            p[p.length] = el;
+    }
+    return p;
 }
 
 let getComponentByName = (list, name) => {
-	let l = list.length ;
-	let right;
-	for(var i = 0 ; i < l ; i ++){
-		el = list[i] ;
-		if(name == el.name){
-			right = el ;
-			break ;
-		} 
-	}
-	return right ;
+    let l = list.length;
+    let right;
+    for (var i = 0; i < l; i++) {
+        el = list[i];
+        if (name == el.name) {
+            right = el;
+            break;
+        }
+    }
+    return right;
 }
 
 
@@ -188,170 +189,171 @@ let getComponentByName = (list, name) => {
 // let loadedLangs = await Object.keys(i18next.services.resourceStore.data) ; 
 
 let params = CONSTANTS.jadeparams = {
-	basedir:jadebasedir + '/',
-	title:CONSTANTS.SITE.title,
-	require:require,
-	render: jade.render,
-	renderFile: jade.renderFile,
-	compile: jade.compile,
-	compileFile: jade.compileFile,
-	join:path.join,
-	app:app,
-	md:md,
-	CDN:CONSTANTS.PATH.cdn,
-	customize:customize,
-	getComponentsByTypename:getComponentsByTypename,
-	getComponentByName:getComponentByName,
-	rfs:rfs,
-	p:p,
-	merge:merge,
-	clone:clone
-} ;
+    basedir: jadebasedir + '/',
+    title: CONSTANTS.SITE.title,
+    require: require,
+    render: jade.render,
+    renderFile: jade.renderFile,
+    compile: jade.compile,
+    compileFile: jade.compileFile,
+    join: path.join,
+    app: app,
+    md: md,
+    CDN: CONSTANTS.PATH.cdn,
+    customize: customize,
+    getComponentsByTypename: getComponentsByTypename,
+    getComponentByName: getComponentByName,
+    rfs: rfs,
+    p: p,
+    merge: merge,
+    clone: clone
+};
 
 
-let topsections, db_sections ;
+let topsections, db_sections;
 
 
 // WWW
-let content = async (req, res) => {
-	
-	// db_sections = db_sections || await fetchdata(req, res, 'datas').catch( err => {console.log(err)}) ;
-	console.log(req.params)
-	if(!!req.params.sectionId){
-		res.render(path.join(__dirname, 'public/jade/content.jade'), merge(params, {
-			lang: req.i18n.language,
-			t: req.t,
-			db_sections:db_sections,
-			topsections:topsections,
-			params:req.params
-		})) ;
-	}else{
-		db_sections = await fetchdata(req, res, 'datas').catch( err => {console.log(err)}) ;
-		
-		res.render(path.join(__dirname, 'public/jade/content.jade'), merge(params, {
-			lang: req.i18n.language,
-			t: req.t,
-			db_sections:db_sections,
-			topsections:topsections,
-			params:req.params
-		})) ;
+let content = async(req, res) => {
 
-	}
+    // db_sections = db_sections || await fetchdata(req, res, 'datas').catch( err => {console.log(err)}) ;
+    // console.log(req.params)
+    if (!!req.params.sectionId) {
+        res.render(path.join(__dirname, 'public/jade/content.jade'), merge(params, {
+            lang: req.i18n.language,
+            t: req.t,
+            db_sections: db_sections,
+            topsections: topsections,
+            params: req.params
+        }));
+    } else {
+        db_sections = await fetchdata(req, res, 'datas').catch(err => { console.log(err) });
+
+        res.render(path.join(__dirname, 'public/jade/content.jade'), merge(params, {
+            lang: req.i18n.language,
+            t: req.t,
+            db_sections: db_sections,
+            topsections: topsections,
+            params: req.params
+        }));
+
+    }
 }
 
 // ERRORS
-let error = async (req, res) => {
-	
-	topsections = topsections || await fetchdata(req, res, 'navdatas').catch( err => {console.log(err)}) ;
-	
-	res.render(path.join(__dirname, 'public/jade/error'), merge(params, {
-		title:'DKT 404',
-		errortype:404,
-		errormessage:'Page Not Found',
-		topsections:topsections
-	})) ;
-	
+let error = async(req, res) => {
+
+    topsections = topsections || await fetchdata(req, res, 'navdatas').catch(err => { console.log(err) });
+
+    res.render(path.join(__dirname, 'public/jade/error'), merge(params, {
+        title: 'DKT 404',
+        errortype: 404,
+        errormessage: 'Page Not Found',
+        topsections: topsections
+    }));
+
 }
 
 
-	// MAIN PAGE
+// MAIN PAGE
 
-let root = async (req, res) => {
-	
-	// topsections = topsections || await fetchdata(req, res, 'navdatas').catch( err => {console.log(err)}) ;
-	topsections = await fetchdata(req, res, 'navdatas').catch( err => {console.log(err)}) ;
-	
-	let loadedLangs = await Object.keys(i18next.services.resourceStore.data) ;
-	
-	res.render(path.join(__dirname, 'public/jade/index'), merge(params, {
-		langs:loadedLangs,
-		lang: req.i18n.language,
-		t: req.t,
-		topsections:topsections
-	})) ;
+let root = async(req, res) => {
 
-} 
+    // topsections = topsections || await fetchdata(req, res, 'navdatas').catch( err => {console.log(err)}) ;
+    topsections = await fetchdata(req, res, 'navdatas').catch(err => { console.log(err) });
+
+    let loadedLangs = await Object.keys(i18next.services.resourceStore.data);
+
+    res.render(path.join(__dirname, 'public/jade/index'), merge(params, {
+        langs: loadedLangs,
+        lang: req.i18n.language,
+        t: req.t,
+        topsections: topsections
+    }));
+
+}
 
 
 
 
 // ROUTES
-app.use('/datas/', async (req, res) => {
-	// console.log('requesting datas')
-	await fetchdata(req, res, 'datas', true).catch( err => {console.log(err)}) ;
-}) ;
+app.use('/datas/', async(req, res) => {
+    // console.log('requesting datas')
+    await fetchdata(req, res, 'datas', true).catch(err => { console.log(err) });
+});
 
-app.use('/navdatas/', async (req, res) => {
-	// console.log('requesting navdatas')
-	await fetchdata(req, res, 'navdatas', true).catch( err => {console.log(err)}) ;
-}) ;
-
-
+app.use('/navdatas/', async(req, res) => {
+    // console.log('requesting navdatas')
+    await fetchdata(req, res, 'navdatas', true).catch(err => { console.log(err) });
+});
 
 
 
-app.use('/404/', async (req, res) => {
-	// console.log('requesting error')
-	await error(req, res, true).catch( err => {console.log(err)}) ;
-}) ;
 
 
-app.use('/content/section/:sectionId', async (req, res) => {
-	console.log('HELLOOO')
-	console.log(req.params)
-	
-	await content(req, res).catch( err => {console.log(err)}) ;
-}) ;
-
-app.use('/content/', async (req, res) => {
-	console.log(req.params)
-	
-	await content(req, res).catch( err => {console.log(err)}) ;
-}) ;
-
-app.use('/', async (req, res) => {
-	// console.log('requesting home') ;
-	await root(req, res).catch( err => {console.log(err)}) ;
-	
-}) ;
+app.use('/404/', async(req, res) => {
+    // console.log('requesting error')
+    await error(req, res, true).catch(err => { console.log(err) });
+});
 
 
-let DELAY_TIME = 1 ;
+app.use('/content/section/:sectionId', async(req, res) => {
+    // console.log(req.params)
 
-if(process.env.SHOULD_DELAY == true || process.env.SHOULD_DELAY >=1 ){
-	console.log('DELAYING '+process.env.SHOULD_DELAY+'MS SERVER START FOR STRAPI TO LOAD') ;
-	DELAY_TIME = process.env.SHOULD_DELAY ;
+    await content(req, res).catch(err => { console.log(err) });
+});
+
+app.use('/content/', async(req, res) => {
+    // console.log(req.params)
+
+    await content(req, res).catch(err => { console.log(err) });
+});
+
+app.use('/', async(req, res) => {
+    // console.log('requesting home') ;
+    await root(req, res).catch(err => { console.log(err) });
+
+});
+
+
+let DELAY_TIME = 1;
+
+if (process.env.SHOULD_DELAY == true || process.env.SHOULD_DELAY >= 1) {
+    console.log('DELAYING ' + process.env.SHOULD_DELAY + 'MS SERVER START FOR STRAPI TO LOAD');
+    DELAY_TIME = process.env.SHOULD_DELAY;
 }
 
-setTimeout(function(){
+setTimeout(function() {
 
 
-	// SERVER LAUNCHING
-	(async (app)=>{
-		let SUCCESS = true ;
-		
-			if(isLive){
-				// SITE LOGIN AS DKT VIEWER USER
-				await login(app).catch((err) => {
-					console.log(err)
-					console.log('/tLOGIN FAILED  ////////') ;
-					console.log('Let us check \n\t1. our Credentials\n	2. if server on "' + CONSTANTS.PATH.db + '" is running') ;
-					SUCCESS = FALSE ;
-					console.log('Server not Ready') ;
-				}) ;
-				
-			}else{
+    // SERVER LAUNCHING
+    (async(app) => {
+        let SUCCESS = true;
 
-			}
+        if (isLive) {
+            // SITE LOGIN AS DKT VIEWER USER
+            await login(app).catch((err) => {
+                console.log(err)
+                console.log('/tLOGIN FAILED  ////////');
+                console.log('Let us check \n\t1. our Credentials\n	2. if server on "' + CONSTANTS.PATH.db + '" is running');
+                SUCCESS = false;
+                console.log('Server not Ready');
+            });
 
-			if (SUCCESS) server.launchServer(app) ;
-			else process.exit() ;
+            console.log('WILL LOAD LIVE MONGO DATAS')
 
-	})(app) ;
+        } else {
+            console.log('WILL LOAD LOCAL FILES')
+        }
+
+        if (SUCCESS) server.launchServer(app);
+        else process.exit();
+
+    })(app);
 
 
 
 
 }, DELAY_TIME)
 
-module.exports = app ;
+module.exports = app;
--- public/js/nodeless_app/events/index.js
+++ public/js/nodeless_app/events/index.js
@@ -1,189 +1,197 @@
-
-
-
 // EVENT HANDLING
-;(function(){
-	
-	
-	// ARROWS
-	
-	var Arrows = Type.define({
-		pkg:'org.libspark.straw::Arrows',
-		domain:Type.appdomain,
-		statics:{
-			keycodes:{
-				'32':'space',
-				'33':'pageup',
-				'34':'pagedown',
-				'35':'end',
-				'36':'home',
-				'37':'left',
-				'38':'up',
-				'39':'right',
-				'40':'down',
-				'45':'insert',
-				'46':'delete',
-				'8':'backspace',
-				'13':'enter',
-				'27':'escape',
-			}
-		},
-		constructor:Arrows = function Arrows(){
-			//
-		},
-		enable:function(closures){
-			this.closures = closures ;
-			
-			return this ;
-		},
-		register:function(){
-			
-			var fff = this;
-			
-			$(document).bind('keydown', function(e){
-				
-				var keycodes 		= Arrows.keycodes,
-					closures 		= fff.closures,
-					keycode 		= e.keyCode,
-					cl ;
-				
-				if(keycode 			in keycodes){
-					if(!!(cl 		= closures[keycodes[keycode]])){
-						cl.apply(ArrowsNavigator.instance, [e]) ;
-					}
-				}
-				
-			}) ;
-			
-			return this ;
-		}
-	})
-	
-	
-	var ArrowsNavigator = Type.define({
-		pkg:'org.libspark.straw::ArrowsNavigator',
-		domain:Type.appdomain,
-		constructor:ArrowsNavigator = function ArrowsNavigator(){
-			ArrowsNavigator.instance = this.enable() ;
-		},
-		getCurrentStep:function(){
-			return AddressHierarchy.instance.currentStep ;
-		},
-		getEligible:function(){
-			
-			var el = this.eligible || AddressHierarchy.instance.currentStep ;
-			
-			if(el.id == "") el = el.parentStep ;
-			
-			return (this.eligible = el) ;
-		},
-		selectNext:function(d){
-			var s = this.getEligible() ;
-			if(!s.parentStep) return s ;
-			return s.parentStep.hasNext() ? s.parentStep.getNext() : s.parentStep.getChild(0) ;
-		},
-		selectPrev:function(d){
-			var s = this.getEligible() ;
-			if(!s.parentStep) return s ;
-			return s.parentStep.hasPrev() ? s.parentStep.getPrev() : s.parentStep.getChild(s.parentStep.children.length - 1) ;
-		},
-		selectUp:function(d){
-			var s = this.getEligible() ;
-			return s.parentStep == Unique.instance ? s : s.parentStep || s ;
-		},
-		selectDown:function(d){
-			var s = this.getEligible() ;
-			return s.defaultStep || s.children[0] || s ;
-		},
-		elect:function(){
-			var s = this.getEligible() ;
+;
+(function() {
 
-			if(s != this.getCurrentStep()) {
-				AddressHierarchy.instance.changer.setStepValue(s) ;
-			}
-		},
-		go:function(step, cur){
-			if(step != cur)
-				AddressHierarchy.instance.changer.setStepValue(step) ;
-		},
-		enable:function(){
 
-			var a = this ;
+    // ARROWS
 
-			var closures = {
-				'left':function(e){
-					var st = a.getCurrentStep() ;
-					var s = st.handleUp() ;
-					this.go(s, st) ;
-				},
-				'up':function(e){
-					var st = a.getCurrentStep() ;
-					var s = st.handlePrev() ;
-					this.go(s, st) ;
-				},
-				'right':function(e){
-					var st = a.getCurrentStep() ;
-					var s = st.handleDown() ;
-					this.go(s, st) ;
-				},
-				'down':function(e){
-					var st = a.getCurrentStep() ;
-					var s = st.handleNext() ;
-					this.go(s, st) ;
-				}
-			}
-			
-			new Arrows().enable(closures).register() ;
-			
-			return this ;
-		}
-		
-	})
-	
-	// ArrowsNavigator.instance = new ArrowsNavigator() ;
-	
-	var halted = false ;
-	
-	var pauseBetween = function(){
-		var AT = BetweenJS.$.AnimationTicker ;
-		if(!!AT.started){
-			if(!!AT.HALT) {
-				halted = false ;
-				AT.restoreSystem() ;
-			} else{
-				AT.haltSystem() ;
-				halted = true ;
-			}
-		}
-	}
-	
-	// SPACEBAR
+    var Arrows = Type.define({
+        pkg: 'org.libspark.straw::Arrows',
+        domain: Type.appdomain,
+        statics: {
+            keycodes: {
+                '32': 'space',
+                '33': 'pageup',
+                '34': 'pagedown',
+                '35': 'end',
+                '36': 'home',
+                '37': 'left',
+                '38': 'up',
+                '39': 'right',
+                '40': 'down',
+                '45': 'insert',
+                '46': 'delete',
+                '8': 'backspace',
+                '13': 'enter',
+                '27': 'escape',
+            }
+        },
+        constructor: Arrows = function Arrows() {
+            //
+        },
+        enable: function(closures) {
+            this.closures = closures;
 
-	$(document).on('keydown', function(e){
-		if(e.keyCode == 32){
-			pauseBetween() ;
-		}
-	})
-	
-	 
-	
-	$(window).on('blur', function(e){
-		var AT = BetweenJS.$.AnimationTicker ;
-		if(!!AT.started && !halted) AT.haltSystem() ;
-		// trace('blurred')
-		
-	})
-	
-	$(window).on('focus', function(e){
-		var AT = BetweenJS.$.AnimationTicker ;
-		if(!!AT.started && !halted) AT.restoreSystem() ;
-		// trace('focused')
-	})
-	
-	
-	
-	
-	 /**/
-	
-	
-})() ;
+            return this;
+        },
+        register: function() {
+
+            var fff = this;
+
+            $(document).bind('keydown', function(e) {
+
+                var keycodes = Arrows.keycodes,
+                    closures = fff.closures,
+                    keycode = e.keyCode,
+                    cl;
+
+                if (keycode in keycodes) {
+                    if (!!(cl = closures[keycodes[keycode]])) {
+                        cl.apply(ArrowsNavigator.instance, [e]);
+                    }
+                }
+
+            });
+
+            return this;
+        }
+    })
+
+
+    var ArrowsNavigator = Type.define({
+        pkg: 'org.libspark.straw::ArrowsNavigator',
+        domain: Type.appdomain,
+        constructor: ArrowsNavigator = function ArrowsNavigator() {
+            ArrowsNavigator.instance = this.enable();
+        },
+        getCurrentStep: function() {
+            return AddressHierarchy.instance.currentStep;
+        },
+        getEligible: function() {
+
+            var el = this.eligible || AddressHierarchy.instance.currentStep;
+
+            if (el.id == "") el = el.parentStep;
+
+            return (this.eligible = el);
+        },
+        selectNext: function(d) {
+            var s = this.getEligible();
+            if (!s.parentStep) return s;
+            return s.parentStep.hasNext() ? s.parentStep.getNext() : s.parentStep.getChild(0);
+        },
+        selectPrev: function(d) {
+            var s = this.getEligible();
+            if (!s.parentStep) return s;
+            return s.parentStep.hasPrev() ? s.parentStep.getPrev() : s.parentStep.getChild(s.parentStep.children.length - 1);
+        },
+        selectUp: function(d) {
+            var s = this.getEligible();
+            return s.parentStep == Unique.instance ? s : s.parentStep || s;
+        },
+        selectDown: function(d) {
+            var s = this.getEligible();
+            return s.defaultStep || s.children[0] || s;
+        },
+        elect: function() {
+            var s = this.getEligible();
+
+            if (s != this.getCurrentStep()) {
+                AddressHierarchy.instance.changer.setStepValue(s);
+            }
+        },
+        go: function(step, cur) {
+            if (step != cur)
+                AddressHierarchy.instance.changer.setStepValue(step);
+        },
+        enable: function() {
+
+            var a = this;
+
+            var closures = {
+                'left': function(e) {
+                    var st = a.getCurrentStep();
+                    var s = st.handleUp();
+                    this.go(s, st);
+                },
+                'up': function(e) {
+                    var st = a.getCurrentStep();
+                    var s = st.handlePrev();
+                    this.go(s, st);
+                },
+                'right': function(e) {
+                    var st = a.getCurrentStep();
+                    var s = st.handleDown();
+                    this.go(s, st);
+                },
+                'down': function(e) {
+                    var st = a.getCurrentStep();
+                    var s = st.handleNext();
+                    this.go(s, st);
+                }
+            }
+
+            new Arrows().enable(closures).register();
+
+            return this;
+        }
+
+    })
+
+    // ArrowsNavigator.instance = new ArrowsNavigator() ;
+
+    var halted = false;
+
+    var pauseBetween = function() {
+        var AT = BetweenJS.$.AnimationTicker;
+        if (!!AT.started) {
+            if (!!AT.HALT) {
+                halted = false;
+                AT.restoreSystem();
+            } else {
+                AT.haltSystem();
+                halted = true;
+            }
+        }
+    }
+
+    // SPACEBAR
+
+    $(document).on('keydown', function(e) {
+        if (e.keyCode == 32) {
+            pauseBetween();
+        }
+    })
+
+
+
+    $(window).on('blur', function(e) {
+        var AT = BetweenJS.$.AnimationTicker;
+        if (!!AT.started && !halted) AT.haltSystem();
+        // trace('blurred')
+
+        // if (window.CURRENTSLIDE) {
+        //     window.CURRENTSLIDE.halt();
+        // }
+
+
+    })
+
+    $(window).on('focus', function(e) {
+        var AT = BetweenJS.$.AnimationTicker;
+        if (!!AT.started && !halted) AT.restoreSystem();
+        // trace('focused')
+
+        // if (window.CURRENTSLIDE) {
+        //     window.CURRENTSLIDE.launch();
+        // }
+
+    })
+
+
+
+
+    /**/
+
+
+})();
